d919d011008ef9784a0758aaf0edf9911aaee6f3
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-07-13 20:19:01 +0300

    removing Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Counter } from "./features/counter/Counter";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
