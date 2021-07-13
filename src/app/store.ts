d35e13f7eda31505c8c714c3c932f70e0d7d5476
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-07-13 19:38:15 +0300

    sperating types from other inputs

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,6 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import type { ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
