a03432a34b52f6e4fe83d6a73d145d189cfd1466
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-07-16 19:37:19 +0300

    removing thunk --- there will be no API calls

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import type { ThunkAction, Action } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
 import companyReducer from "../features/companySlice";
 
@@ -9,9 +8,3 @@ export const store = configureStore({
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
