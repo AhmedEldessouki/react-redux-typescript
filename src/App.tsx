60ccf83d8b631d5eab14baf5c2afd3c24d2bcb8b
Ahmed eldessouki <nemoahmed@hotmail.co.uk>
2021-07-16 19:27:52 +0300

    moving Redux store and provider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from "react";
-import Home from "./pages/home";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 import { useAuth } from "./context/authContext";
-import "./App.css";
+import Home from "./pages/home";
 import SignIn from "./components/signin";
+import "./App.css";
 
 function App() {
   const { user } = useAuth();
-  return <div className="App">{user ? <Home /> : <SignIn />}</div>;
+  return (
+    <div className="App">
+      {user ? (
+        <Provider store={store}>
+          <Home />
+        </Provider>
+      ) : (
+        <SignIn />
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import AuthProvider from "./context/authContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
